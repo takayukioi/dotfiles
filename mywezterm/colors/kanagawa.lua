dcecdde9006e5f54dc4a19292a5a352f1dd47b69
renoinn <oomori.lunch@gmail.com>
2026-06-17 21:17:03 +0900

    feat: add kanagawa

---
+++ mywezterm/colors/kanagawa.lua
@@ -0,0 +1,134 @@
+
+local M = {}
+M.kanagawa = {
+  foreground = '#dcd7ba',
+  background = '#1f1f28',
+    ansi = {
+        black = '#090618', -- black
+        red = '#c34043', -- red
+        green = '#76946a', -- green
+        yellow = '#c0a36e', -- yellow
+        blue = '#7e9cd8', -- blue
+        magenta = '#957fb8', -- magenta
+        cyan = '#6a9589', -- cyan
+        white = '#c8c093', -- white
+    },
+    bright = {
+        black = '#727169', -- bright black
+        red = '#e82424', -- bright red
+        green = '#98bb6c', -- bright green
+        yellow = '#e6c384', -- bright yellow
+        blue = '#7fb4ca', -- bright blue
+        magenta = '#938aa9', -- bright magenta
+        cyan = '#7aa89f', -- bright cyan
+        white = '#dcd7ba', -- bright white
+    }
+}
+
+M.colors = {
+  foreground = M.kanagawa.foreground,
+  background = M.kanagawa.background,
+
+  cursor_bg = M.kanagawa.foreground,
+  cursor_fg = M.kanagawa.background,
+  cursor_border = M.kanagawa.foreground,
+
+ansi = {
+  M.kanagawa.ansi.black,
+  M.kanagawa.ansi.red,
+  M.kanagawa.ansi.green,
+  M.kanagawa.ansi.yellow,
+  M.kanagawa.ansi.blue,
+  M.kanagawa.ansi.magenta,
+  M.kanagawa.ansi.cyan,
+  M.kanagawa.ansi.white,
+},
+
+brights = {
+  M.kanagawa.bright.black,
+  M.kanagawa.bright.red,
+  M.kanagawa.bright.green,
+  M.kanagawa.bright.yellow,
+  M.kanagawa.bright.blue,
+  M.kanagawa.bright.magenta,
+  M.kanagawa.bright.cyan,
+  M.kanagawa.bright.white,
+},
+
+  selection_bg = M.kanagawa.bright.blue,
+  selection_fg = M.kanagawa.ansi.black,
+  
+  scrollbar_thumb = M.kanagawa.bright.white,
+
+  split = M.kanagawa.bright.white,
+
+  indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+
+  -- -- Since: 20220319-142410-0fcdea07
+  -- -- When the IME, a dead key or a leader key are being processed and are effectively
+  -- -- holding input pending the result of input composition, change the cursor
+  -- -- to this color to give a visual cue about the compose state.
+  -- compose_cursor = 'orange',
+
+  -- copy_mode_active_highlight_bg = { Color = M.kanagawa.bright.green },
+  -- copy_mode_active_highlight_fg = { AnsiColor = M.kanagawa.ansi.black },
+  -- copy_mode_inactive_highlight_bg = { Color = M.kanagawa.bright.black },
+  -- copy_mode_inactive_highlight_fg = { AnsiColor = M.kanagawa.ansi.white },
+
+  -- quick_select_label_bg = { Color = 'peru' },
+  -- quick_select_label_fg = { Color = '#ffffff' },
+  -- quick_select_match_bg = { AnsiColor = 'Navy' },
+  -- quick_select_match_fg = { Color = '#ffffff' },
+
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = M.kanagawa.background,
+
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = M.kanagawa.background,
+      -- The color of the text for the tab
+      fg_color = M.kanagawa.foreground,
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+    new_tab = {
+      bg_color = M.kanagawa.background,
+      fg_color = M.kanagawa.foreground,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over the new tab button
+    new_tab_hover = {
+      bg_color = M.kanagawa.background,
+      fg_color = M.kanagawa.foreground,
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `new_tab_hover`.
+    },
+  },
+}
+
+return M
