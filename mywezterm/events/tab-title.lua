5316555cf497900a8ddc37c25b2a6aa5e7d8796b
renoinn <oomori.lunch@gmail.com>
2026-06-19 11:18:06 +0900

    feat: add tab icons

--- mywezterm/events/tab-title.lua
+++ mywezterm/events/tab-title.lua
@@ -1,5 +1,74 @@
 local wezterm = require("wezterm")
 
+-- tab icons
+local TAB_ICON_DOCKER = wezterm.nerdfonts.md_docker
+local TAB_ICON_PYTHON = wezterm.nerdfonts.dev_python
+local TAB_ICON_NEOVIM = wezterm.nerdfonts.linux_neovim
+local TAB_ICON_ZSH = wezterm.nerdfonts.dev_terminal
+local TAB_ICON_TASK = wezterm.nerdfonts.cod_server_process
+local TAB_ICON_NODE = wezterm.nerdfonts.md_language_typescript
+local TAB_ICON_RAILS = wezterm.nerdfonts.dev_rails
+local TAB_ICON_FALLBACK = wezterm.nerdfonts.md_console_line
+
+-- tab icon colors
+local TAB_ICON_COLOR_DOCKER = "#4169e1"
+local TAB_ICON_COLOR_PYTHON = "#ffd700"
+local TAB_ICON_COLOR_NEOVIM = "#32cd32"
+local TAB_ICON_COLOR_ZSH = "#1d5f7a"
+local TAB_ICON_COLOR_TASK = "#ff7f50"
+local TAB_ICON_COLOR_NODE = "#339933"
+local TAB_ICON_COLOR_RAILS = "#cc0000"
+local TAB_ICON_COLOR_FALLBACK = "#090618"
+
+local cwd_cache = {}
+function get_icon(tab_info)
+	local icon = TAB_ICON_FALLBACK
+	local icon_foreground = TAB_ICON_COLOR_FALLBACK
+	if tab_info.active_pane.title == "nvim" then
+		icon = TAB_ICON_NEOVIM
+		icon_foreground = TAB_ICON_COLOR_NEOVIM
+	elseif tab_info.active_pane.title == "zsh" then
+		icon = TAB_ICON_ZSH
+		icon_foreground = TAB_ICON_COLOR_ZSH
+	elseif tab_info.active_pane.title == "Python" or string.find(tab_info.active_pane.title, "python") then
+		icon = TAB_ICON_PYTHON
+		icon_foreground = TAB_ICON_COLOR_PYTHON
+	elseif tab_info.active_pane.title == "node" or string.find(tab_info.active_pane.title, "node") then
+		icon = TAB_ICON_NODE
+		icon_foreground = TAB_ICON_COLOR_NODE
+	elseif tab_info.active_pane.title == "docker" or string.find(tab_info.active_pane.title, "docker") then
+		icon = TAB_ICON_DOCKER
+		icon_foreground = TAB_ICON_COLOR_DOCKER
+	elseif tab_info.active_pane.title == "task" or string.find(tab_info.active_pane.title, "task") then
+		icon = TAB_ICON_TASK
+		icon_foreground = TAB_ICON_COLOR_TASK
+	elseif tab_info.active_pane.title == "rails" or string.find(tab_info.active_pane.title, "rails") then
+		icon = TAB_ICON_RAILS
+		icon_foreground = TAB_ICON_COLOR_RAILS
+	end
+	return {
+		icon = icon,
+		foreground = icon_foreground,
+	}
+end
+function tab_title(tab_info)
+  	local pane_id = tab_info.active_pane.pane_id
+	local dir_name = cwd_cache[pane_id] or tab_info.active_pane.title
+
+	local title = tab_info.tab_title
+	if dir_name and #dir_name > 0 then
+		title = string.format(" %s ", dir_name)
+	end
+
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
 -- The filled in variant of the < symbol
 local SOLID_LEFT_ARROW = utf8.char(0xe0ba)
 local SOLID_LEFT_MOST = utf8.char(0x2588)
@@ -7,8 +76,6 @@ local SOLID_LEFT_MOST = utf8.char(0x2588)
 -- The filled in variant of the > symbol
 local SOLID_RIGHT_ARROW = utf8.char(0xe0bc)
 
--- カレントディレクトリを保持するキャッシュテーブル
-local cwd_cache = {}
 local M = {}
 M.setup = function(color)
 	-- 1. update-status イベントでCWDを定期的に取得してキャッシュに保存
@@ -38,8 +105,9 @@ M.setup = function(color)
 		end
 
 		local edge_foreground = background
+		local icon_info = get_icon(tab)
 		local clean_title = tab_title(tab)
-		local title = " " .. wezterm.truncate_right(clean_title, max_width) .. " "
+		local title = wezterm.truncate_right(clean_title, max_width)
 		local left_arrow = SOLID_LEFT_ARROW
 		if tab.tab_index == 0 then
 			left_arrow = SOLID_LEFT_MOST
@@ -49,33 +117,18 @@ M.setup = function(color)
 			{ Background = { Color = edge_background } },
 			{ Foreground = { Color = edge_foreground } },
 			{ Text = left_arrow },
+			{ Foreground = { Color = icon_info.foreground } },
+			{ Background = { Color = background } },
+    { Text = " " .. icon_info.icon },
 			{ Background = { Color = background } },
 			{ Foreground = { Color = foreground } },
-			{ Text = title },
+			{ Text = title .. " " },
 			{ Background = { Color = edge_background } },
 			{ Foreground = { Color = edge_foreground } },
 			{ Text = SOLID_RIGHT_ARROW },
 			{ Attribute = { Intensity = "Normal" } },
 		}
 	end)
-end
-
-function tab_title(tab_info)
-	local pane_id = tab_info.active_pane.pane_id
-	local dir_name = cwd_cache[pane_id] or tab_info.active_pane.title
-
-	local title = tab_info.tab_title
-	if dir_name and #dir_name > 0 then
-		title = string.format(" %d. %s ", tab_info.tab_index + 1, dir_name)
-	end
-
-	-- if the tab title is explicitly set, take that
-	if title and #title > 0 then
-		return title
-	end
-	-- Otherwise, use the title from the active pane
-	-- in that tab
-	return tab_info.active_pane.title
 end
 
 return M
